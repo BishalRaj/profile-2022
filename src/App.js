005bca40843317a331cc0c6c6742369284b4a7ff
BishalRaj <bisal1shakya@gmail.com>
2022-10-01 12:56:18 +0100

    fixed login page form

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   return <Routes>
     <Route path='/' element={<Auth />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/home" element={<Register />} />
   </Routes>
 }
 
--- src/layout/authLayout/login.jsx
+++ src/layout/authLayout/login.jsx
@@ -1,29 +1,43 @@
+import axios from "axios";
 import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  var hasError = false;
+
+  const submitData = () => {
+    axios.get("/test").then((data) => {
+      console.log(data);
+    });
+  };
+
   return (
     <Container className="vh-100">
       <Row className="h-100 ">
-        <Col sm={5} className="mx-auto my-auto shadow px-5 py-5 rounded">
+        <Col
+          sm={10}
+          lg={5}
+          className="mx-auto my-auto shadow px-5 py-5 rounded"
+        >
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              {hasError ? (
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              ) : (
+                ""
+              )}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
+            <Button variant="primary" onClick={submitData}>
+              Login
             </Button>{" "}
             <br />
             <small>or</small> <br />
